9a038577406958e4133381d89f472e46a7fc5822
bruno987 <bruno987silva@gmail.com>
2021-06-20 12:49:45 +0100

    remove rigidbody from camera

--- Assets/Scripts/SchipRailSystem.cs
+++ Assets/Scripts/SchipRailSystem.cs
@@ -4,34 +4,42 @@ using UnityEngine;
 
 public class SchipRailSystem : MonoBehaviour
 {
-    [SerializeField] List<Transform> initialValues = new List<Transform>();
     [SerializeField] Queue<Transform> rail;
     [SerializeField] Transform Ship;
+    [SerializeField] ModuleManager moduleManager;
     Rigidbody rb;
     float speed = 3f;
     float distanceFromShip = 0;
     // Start is called before the first frame update
     void Start()
     {
-        distanceFromShip = Vector3.Distance(transform.position,Ship.position);
-        rb = GetComponent<Rigidbody>();
         rail = new Queue<Transform>();
-        foreach (var position in initialValues)
+        foreach (var railPoint in moduleManager.nextModulePoints())
         {
-            rail.Enqueue(position);
+            rail.Enqueue(railPoint);
         }
-        rb.velocity = (rail.Peek().position - transform.position).normalized * speed; 
+
+        distanceFromShip = Vector3.Distance(transform.position,Ship.position);
+        transform.Translate((rail.Peek().position - transform.position).normalized * speed * Time.deltaTime, Space.World); 
     }
 
-    // Update is called once per frame
+    //fazer logica dos forks
     void Update()
     {
         if (rail.Count > 0)
         {
+            if (rail.Count == 1)
+            {
+                moduleManager.GenerateRandomModule();
+                foreach (var railPoint in moduleManager.nextModulePoints())
+                {
+                    rail.Enqueue(railPoint);
+                }
+            }
             var CurPos = transform.position + transform.forward*distanceFromShip;
             var ShipPos = new Vector2(CurPos.x,CurPos.z);
             var TargPos = new Vector2(rail.Peek().position.x,rail.Peek().position.z);
-            if (Vector2.Distance(ShipPos,TargPos) <= .5f)
+            if (Vector2.Distance(ShipPos,TargPos) <= 1f)
             {
                 Debug.Log("Remove point");
                 RemoveFirstPoint();
@@ -39,13 +47,19 @@ public class SchipRailSystem : MonoBehaviour
                 float angle = Vector3.SignedAngle(targetDirection,Ship.forward,Vector3.up); 
                 //fazer um lerp nisto
                 transform.RotateAround(Ship.position,Vector3.up,-angle);     
-                rb.velocity = (rail.Peek().position - transform.position).normalized * speed;          
             }
+            transform.Translate((rail.Peek().position - transform.position).normalized * speed * Time.deltaTime, Space.World);        
             
         }
     }
 
     void RemoveFirstPoint(){
         rail.Dequeue();
+    }
+
+    //call module manager to give the queue of railpoints 
+    //corresponding to the side of the fork the player chose
+    private void OnTriggerEnter(Collider other) {
+    
     }
 }
